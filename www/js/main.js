7b147532e5619b63cc704e4fa4f98b47a4fcfcce
Wang Xiaoqiang - xiwang <xiaoqiang.wang@acxiom.com>
2015-03-09 18:25:50 +0800

    make it work only with require.js and jquery.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,24 +1,8 @@
 requirejs.config({
   baseUrl: 'js',
-  shim: {
-    underscore: {
-      exports: '_'
-    },
-    backbone: {
-      deps: [ 'underscore', 'jquery' ],
-      exports: 'Backbone'
-    },
-    backboneLocalstorage: {
-      deps: [ 'backbone' ],
-      exports: 'Store'
-    }
-  },
   paths: {
     jquery: 'vendor/jquery-1.11.2.min',
-    modernizr: 'vendor/modernizr-2.8.3.min',
-    underscore: 'http://underscorejs.org/underscore-min',
-    backbone: 'http://backbonejs.org/backbone-min',
-    backboneLocalstorage: 'https://raw.githubusercontent.com/jeromegn/Backbone.localStorage/master/backbone.localStorage-min'
+    modernizr: 'vendor/modernizr-2.8.3.min'
   },
   map: {
     '*': {
@@ -28,7 +12,7 @@ requirejs.config({
 });
 
 require([
-  'modernizr', 'plugins', 'backbone',
+  'modernizr', 'plugins', 'jquery',
   'loadCSS!css/normalize.css',
   'loadCSS!css/main.css'
 ], function(){
