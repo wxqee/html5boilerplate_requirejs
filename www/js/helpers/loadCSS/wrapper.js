ac0e6682417a34efa00b6508ef8de6b8f8982ec4
Wang Xiaoqiang - xiwang <xiaoqiang.wang@acxiom.com>
2015-03-09 18:20:31 +0800

    fix bug: css load complete but not registerred. support Backbone, Underscore and Backbone LocalStorage

--- www/js/helpers/loadCSS/wrapper.js
+++ www/js/helpers/loadCSS/wrapper.js
@@ -1,7 +1,10 @@
 define({
   load: function (name, req, onload, config) {
     req([ config.baseUrl + 'helpers/loadCSS/loadCSS.js' ], function(){
-      loadCSS( name );
+      loadCSS( name, null, null, function() {
+        console.log(name+' is loaded.');
+        onload(name);
+      } );
     });
   }
 });
--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,24 @@
 requirejs.config({
   baseUrl: 'js',
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: [ 'underscore', 'jquery' ],
+      exports: 'Backbone'
+    },
+    backboneLocalstorage: {
+      deps: [ 'backbone' ],
+      exports: 'Store'
+    }
+  },
   paths: {
     jquery: 'vendor/jquery-1.11.2.min',
-    modernizr: 'vendor/modernizr-2.8.3.min'
+    modernizr: 'vendor/modernizr-2.8.3.min',
+    underscore: 'http://underscorejs.org/underscore-min',
+    backbone: 'http://backbonejs.org/backbone-min',
+    backboneLocalstorage: 'https://raw.githubusercontent.com/jeromegn/Backbone.localStorage/master/backbone.localStorage-min'
   },
   map: {
     '*': {
@@ -12,11 +28,9 @@ requirejs.config({
 });
 
 require([
-  'jquery', 'modernizr', 'plugins',
+  'modernizr', 'plugins', 'backbone',
   'loadCSS!css/normalize.css',
   'loadCSS!css/main.css'
-], function( $ ){
-  $(document).ready(function(){
-    console.log('Hello, world.');
-  });
+], function(){
+  console.log( 'Hello, world' );
 });
