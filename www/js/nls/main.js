c1860f8a7e08121190f24c4b8947946f897ea128
Wang Xiaoqiang - xiwang <xiaoqiang.wang@acxiom.com>
2015-03-10 15:14:11 +0800

    add nls

---
+++ www/js/nls/main.js
@@ -0,0 +1,7 @@
+define({
+  "root": {
+    "hi": "Hello world!",
+    "whoami": "This is HTML5 Boilerplate."
+  },
+  "zh-cn": true
+});
---
+++ www/js/nls/zh-cn/main.js
@@ -0,0 +1,4 @@
+define({
+  "hi": "你好，世界！",
+  "whoami": "我是 HTML5 Boilerplate"
+});
