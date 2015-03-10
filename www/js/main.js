7b037486742b376d0396b18de045e4f83860599f
Wang Xiaoqiang - xiwang <xiaoqiang.wang@acxiom.com>
2015-03-10 14:26:59 +0800

    show apps in index.html

--- www/js/main.js
+++ www/js/main.js
@@ -2,13 +2,26 @@
 require([ 'require-config' ], function() {
 
 require([
-  'jquery',
   'modernizr',
   'plugins',
   'loadCSS!css/normalize.css',
   'loadCSS!css/main.css',
-], function( $ ){
-  $('body').html( "<h1>Hello, world. I'm (HTML5 Boilerplate, RequireJS).</h1>" );
+], function(){
+  var apps, target, htmls;
+  apps = [
+    { name: "todo", subject: "TodoMVC" }
+  ];
+
+  htmls = [ "<h1>Hello, world. I'm HTML5 Boilerplate</h1>" ]
+  htmls.push( '<h3>Apps</h3>' );
+  htmls.push( "<ul>" );
+  apps.forEach(function( app, i ){
+    htmls.push( '<li><a href="'+ app.name +'.html" target="_blank">'+ app.subject +'</li>' );
+  });
+  htmls.push( "</ul>" );
+
+  target = document.querySelectorAll("body")[0];
+  target.innerHTML = htmls.join( "" );
 });
 
 });  // require-config.js
